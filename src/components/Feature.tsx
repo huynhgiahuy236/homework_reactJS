7f65b93dcd21800939ec93a8930ce585b5e957fd
huynhgiahuy <huynhgiahuy236@gmail.com>
2026-05-12 23:39:24 +0700

    React_Buoi_1

--- src/components/Feature.tsx
+++ src/components/Feature.tsx
@@ -1,5 +1,5 @@
-import React from "react";
-import { featureData } from "../data/feature";
+import React, { useState } from "react";
+import  featureData  from "../data/feature.json";
 
 const Feature = () => {
   return (
